51bf1af96aba370782b0d14f32a00bdeaeb464e3
Jayden Houghton <jayden-houghton@live.nmit.ac.nz>
2023-07-24 13:44:43 +1200

    increase window and image size

--- SimpleGame.py
+++ SimpleGame.py
@@ -67,10 +67,10 @@ def make_a_window():
         key='-IN-', size=(20, 1), font='Any 14')]
     buttons = [sg.Button('Enter',  bind_return_key=True), sg.Button('Exit')]
     command_col = sg.Column([prompt_input, buttons], element_justification='r')
-    layout = [[sg.Image(r'images/forest.png', size=(200, 200), key="-IMG-"), sg.Text(show_current_place(), size=(100, 4), font='Any 12', key='-OUTPUT-')],
-              [command_col]]
-
-    return sg.Window('Adventure Game', layout, size=(500, 300))
+    layout = [[sg.Image(r'images/forest.png', size=(300, 300), key="-IMG-"), 
+                sg.Text(show_current_place(), size=(100, 6), font='Any 12', 
+                key='-OUTPUT-')], [command_col]]
+    return sg.Window('Adventure Game', layout, size=(600, 400))
 
 
 if __name__ == "__main__":
@@ -89,7 +89,7 @@ if __name__ == "__main__":
                 window['-OUTPUT-'].update(current_story)
 
             window['-IMG-'].update(r'images/'+game_locations[current_location]
-                                   ['Image'], size=(100, 100))
+                                   ['Image'], size=(300, 300))
             pass
         elif event == 'Exit' or event is None or event == sg.WIN_CLOSED:
             break
