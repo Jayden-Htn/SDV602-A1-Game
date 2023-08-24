60e7c68aa641c0658beabfab7bc76b1b922638bc
Jayden Houghton <jayden-houghton@live.nmit.ac.nz>
2023-08-24 13:06:27 +1200

    change black formatter line length

--- README.md
+++ README.md
@@ -1,51 +1,60 @@
 # SDV602-A1-Game
+
 Python game for SDV602 assessment 1. Please read this document before setting up and playing the game.
 <br><br>
 
 ## Dependancies
+
 - Python V3.11.4
 - PySimpleGUI V4.60.5
 - PSG-Reskinner V2.3.8
 - Colour V0.1.5
 
 These can be installed by running the command `pip install -r requirements.txt` in the terminal.
+
+Note: the black formatter has been used in this project, but it should not be necessary to install.
 <br><br>
 
 ## Installation
 
 How to run the game:
+
 1. Download the repository and open in VS Code.
 2. Download the dependancies (see above).
 3. Run the SimpleGame.py script.
-<br><br>
+   <br><br>
 
 ## Gameplay Commands
 
 Gameplay is based of entering keywords. Only the first keyword will be processed each turn.
 
 ### Used at any time:
+
 - Inventory
 
 ### Used in specific locations (indicated):
+
 - North, south, east, west
 - Enter, leave, escape
 - Fist, sword, crystal
 - Talk, search, pickup
-<br><br>
+  <br><br>
 
 ## Story
 
 <b>Warning, story spoilers ahead.</b>
 
-There are two phases that must be completed in order and items that must be completed in order to successfully complete the game. 
+There are two phases that must be completed in order and items that must be completed in order to successfully complete the game.
 
 ### Phase 1 (in any order)
+
 - Visit the village to learn the story
 - Collect the key from the coast
 - Collect the sword and crystal (optional) from the cave
 
 ### Phase 2 (boss fight)
-- Enter the castle. 
+
+- Enter the castle.
 - While the castle can be enter once the key is collected, fighting with no weapons will end in death.
 - It is possible to win the fight with just the sword, but not guarenteed.
 - Using the crystal then sword will guarentee victory
--- SimpleGame.py
+++ SimpleGame.py
@@ -1,11 +1,16 @@
-""" 
-The main module of the game.  This module will be responsible for the game loop and the game window.
+"""This is the main module for the game and handles the game window.
+
+Functions:
+    make_a_window()
 """
+
+# Import modules
 import PySimpleGUI as PSG
 import command_parser.command_manager as CMD
 import psg_reskinner as RSKN
 
 
+# Functions
 def make_a_window():
     """
     Creates a game window
@@ -17,15 +22,20 @@ def make_a_window():
     PSG.theme("DarkGreen")  # please make your windows
     prompt_input = [
         PSG.Text("Enter your command", font="Any 14"),
-        PSG.Input(key="-IN-", size=(20, 1), font="Any 14", do_not_clear=False),
+        PSG.Input(key="-IN-", size=(20, 1), 
+                  font="Any 14", do_not_clear=False),
     ]
     buttons = [PSG.Button("Enter", bind_return_key=True), PSG.Button("Exit")]
-    command_col = PSG.Column([prompt_input, buttons], element_justification="r")
+    command_col = PSG.Column([prompt_input, buttons], 
+                             element_justification="r")
     layout = [
         [
             PSG.Image(r"images/forest.png", size=(300, 300), key="-IMG-"),
             PSG.Text(
-                CMD.show_current_place(), size=(39, 12), font="Any 12", key="-OUTPUT-"
+                CMD.show_current_place(), 
+                size=(39, 12), 
+                font="Any 12", 
+                key="-OUTPUT-"
             ),
         ],
         [command_col],
@@ -46,7 +56,9 @@ if __name__ == "__main__":
             window["-OUTPUT-"].update(current_story)
             # update the image
             window["-IMG-"].update(
-                r"images/" + CMD.game_places[CMD.game_state]["Image"], size=(300, 300)
+                r"images/" 
+                + CMD.game_places[CMD.game_state]["Image"], 
+                size=(300, 300)
             )
             # update background with PSG reskinner
             new_theme = CMD.game_places[CMD.game_state]["Theme"]
